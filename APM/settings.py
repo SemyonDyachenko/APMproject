57f7e767df1584592724cb533dc118fe2337342a
Semyon Dyachenko <semyondyachenko@gmail.com>
2023-03-20 23:41:21 +0300

    Update settings.py

--- APM/settings.py
+++ APM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=bccma9n8bmu*b$o*r&t#3-&ca61-ih(irmirnx!wiz!6lmy7o'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
